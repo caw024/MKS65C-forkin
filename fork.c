682b763f71ed4480f862c3b1c5184c283b71222c
Calvin Aw <caw@stuy.edu>
2018-11-18 18:35:58 -0500

    funky

--- fork.c
+++ fork.c
@@ -5,30 +5,48 @@
 #include <fcntl.h>
 #include <sys/types.h>
 
+//I usedd GeeksforGeeks to better understand how fork and wait works.
+//Also looked at simon's post on qaf to see how to do multiple forks at once.
+
 int main(){
-  int child1 = fork();
-  // int child2 = fork();
-  int fd;
+  int randfd;
+  int arr[0];
+  int stat;
+  int l;
+  int m;
+  int i = 0;
 
-  printf("pid: %d \n", getpid());
+ 
+  printf("beginning of parent\n");
 
-  if (child1 == 0){
-    fd = open("/dev/random", O_RDONLY);
-    printf("fd: %d\n", fd);
+  for  (i; i < 2; i++){
+    //child process
+    if (fork() == 0){
+      randfd = open("/dev/random", O_RDONLY);
+      l = read(randfd, arr, sizeof(int));
+      m = close(randfd);
+  
+      l = (l % 15) + 5;
+      sleep(l);
+      printf("child finished sleeping\n");
 
-    sleep((fd % 15) + 5);
-    close(fd);
-    printf("child finished waiting\n");
+      printf("The child slept for %d seconds\n", l);
+      return 0;
+    }
+
+    //parent process
+    else{
+      printf("parent is waiting for child to finish:\n");
+      waitpid(-1,&stat,0);
+      if (WIFEXITED(stat)){
+	printf("parent is done waiting\n");
+      }
+    }
 
   }
-  else{
-    printf("waiting for child to finish:\n");
-    int * a = wait(2);
-    printf("parent is done\n");
+
     
-  }
-
-
+  
 }
 
 
